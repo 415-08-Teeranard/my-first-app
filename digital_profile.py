9e2bd5c4cc4d1ac421459515793d567097be748a
415-08-Teeranard <54283@yupparaj.ac.th>
2026-06-25 09:00:02 +0700

    Create digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("My Digital Profile")
+
+st.header("แนะนำตัว")
+st.writer("นางสาวธีรนาฎ เพ็งปาน")
+st.writer("ชื่อเล่น น้ำตาล")
+st.writer("เลขที่ 08 ม.415")
+
+st.header("เป้าหมายและสิ่งที่อยากทำในอนาคต")
+st.writer("เป้าหมาย: อยากไปเรียนต่อต่างประเทศ")
+st.writer("สิ่งที่อยากเป็นในอนาคต: อยากทำธุรกิจของตัวเอง")
