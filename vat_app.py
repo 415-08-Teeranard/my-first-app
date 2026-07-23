a71a07820772a066dd0736d7b054866f191574d8
415-08-Teeranard <54283@yupparaj.ac.th>
2026-07-23 09:10:16 +0700

    Create vat_app.py

---
+++ vat_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT7%")
+price=stt.number_input("กรอกราคาสินค้า(บาท):",value=0.0)
+vat=price*0.07
+net_price=price-vat
+st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
+st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+st.divider()
+st.write("นางสาวธีรนาฎ เพ็งปาน เลขที่ 8  ม.4/15")
